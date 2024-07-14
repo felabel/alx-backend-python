bd73d5a8a37c3840aa7904f5d429538b1b511c8c
felabel <felicityabel2016@gmail.com>
2024-07-14 14:18:47 +0100

    task 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
